0f370be2e654d82f74c3710fda269fb1dd7b32e4
sjodie1157 <sjodie1157@gmail.com>
2023-12-13 10:57:49 +0200

    added html

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,31 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
+  <div>
+    <form @submit="sendInfo">
+      <div>
+        <label>
+          Enter a value:
+        </label>
+        <input type="text" placeholder="Enter a value" v-model="currValue" @keyup="popUpMessage(currValue)">
+      </div>
+      <div>
+        <h2>
+          List of entered values
+        </h2>
+        <ul v-show="listOfValues.length">
+          <li v-for="data, index in listOfValues" :key="index">
+            <p>{{ data }} @ position {{ index }}</p>
+          </li>
+        </ul>
+      </div>
+      <button type="submit">Display</button>
+    </form>
+  </div>
 </template>
 
 <script>
 export default {
   name: 'App',
-  components: {
-  }
+  components: {}
 }
 </script>
 
